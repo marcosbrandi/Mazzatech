256e68d2252fe8e1191a86a708752a4512ee7a35
Marcos Brandi Torres <marcosbrandi@hotmail.com>
2019-03-31 15:48:09 -0300

    Add files via upload

---
+++ GOL.Api/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GOL.Api.Models
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public virtual Guid Id { get; set; }
+    }
+}
